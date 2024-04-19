ab7a07014fdf9b176984881b3dab0d4c4ae97ec2
PatriciaG <patriciagarcia.2530656@gmail.com>
2024-04-19 02:48:09 -0400

    Added my project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+	<div  className = "bg-gray-200" >
+		<Component {...pageProps} />;
+  </div >
+	)
 }
